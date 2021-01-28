cbfacf3a2a9d81d809481b65619e4fe8db71c0f6
nisabalci <nisabalci00@gmail.com>
2021-01-29 02:08:27 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Laptop";
+            product1.UnitPrice = 5000;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product
+            {
+                Id = 2,
+                CategoryId = 5,
+                UnitsInStock = 5,
+                ProductName = "Telephone",
+                UnitPrice = 2000
+
+            };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+        }
+    }
+}
